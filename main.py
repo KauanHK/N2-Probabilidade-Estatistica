66f62a5d18e3e9ea5ce63c09fc44d52a1c5c8359
KauanHK <kauankaestner06@gmail.com>
2025-05-17 16:41:11 -0300

    data.csv

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
 
 
-# Carregar os dados do CSV
-df = pd.read_csv('Student_Performance.csv')
+def create_fig(df: pd.DataFrame, column: str) -> None:
 
-# Criar o gráfico apenas com a linha de regressão
-plt.figure(figsize=(8, 6))
-sns.regplot(
-    data=df,
-    x='Hours Studied',
-    y='Performance Index',
-    scatter=False,               # Não exibir os pontos
-    line_kws={'color': 'red'}    # Cor da linha de regressão
-)
+    plt.figure(figsize=(8, 6))
+    sns.regplot(
+        data = df,
+        x = column,
+        y = 'total',
+        scatter_kws = {'s': 1},
+        line_kws = {'color': 'red'}
+    )
 
-# Adicionar título e rótulos
-plt.title('Linha de Regressão entre Horas Estudadas e Índice de Performance', fontsize=14)
-plt.xlabel('Horas Estudadas', fontsize=12)
-plt.ylabel('Índice de Performance', fontsize=12)
-plt.tight_layout()
-plt.savefig('fig.png')
+    plt.title(f'Regressão: {column} vs Valor Total', fontsize = 14)
+    plt.xlabel(column, fontsize = 12)
+    plt.ylabel('Valor Total (R$)', fontsize = 12)
+    plt.tight_layout()
+
+    print(f'Salvando gráfico {column} ...')
+    plt.savefig(os.path.join('data', f'{column}.png'))
+
+
+def main() -> None:
+
+    df = pd.read_csv(os.path.join('data', 'data.csv'))
+    df = df.drop(columns = ['id', 'city', 'animal', 'furniture', 'hoa', 'rent amount', 'property tax', 'fire insurance'])
+
+    for column in df.columns:
+        create_fig(df, column)
+
+
+if __name__ == '__main__':
+    main()
