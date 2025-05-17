1cbc756641cd468a3d919009a9f05c2a8ea7b8c8
KauanHK <kauankaestner06@gmail.com>
2025-05-17 16:48:27 -0300

    Gerados gráficos

--- main.py
+++ main.py
@@ -4,33 +4,41 @@ import matplotlib.pyplot as plt
 import os
 
 
-def create_fig(df: pd.DataFrame, column: str) -> None:
+def create_fig(df: pd.DataFrame, x: str, y: str) -> None:
+
+    print(f'Criando gráfico {x} vs {y}...')
 
     plt.figure(figsize=(8, 6))
     sns.regplot(
         data = df,
-        x = column,
-        y = 'total',
+        x = x,
+        y = y,
         scatter_kws = {'s': 1},
         line_kws = {'color': 'red'}
     )
 
-    plt.title(f'Regressão: {column} vs Valor Total', fontsize = 14)
-    plt.xlabel(column, fontsize = 12)
+    plt.title(f'Regressão: {x} vs {y}', fontsize = 14)
+    plt.xlabel(x, fontsize = 12)
     plt.ylabel('Valor Total (R$)', fontsize = 12)
     plt.tight_layout()
 
-    print(f'Salvando gráfico {column} ...')
-    plt.savefig(os.path.join('data', f'{column}.png'))
+    print(f'Salvando gráfico {x} ...')
+    plt.savefig(os.path.join('data', f'{x}.png'))
 
 
 def main() -> None:
 
+    print('Lendo tabela...')
     df = pd.read_csv(os.path.join('data', 'data.csv'))
-    df = df.drop(columns = ['id', 'city', 'animal', 'furniture', 'hoa', 'rent amount', 'property tax', 'fire insurance'])
 
-    for column in df.columns:
-        create_fig(df, column)
+    print('Tratando os dados...')
+    df = df.drop(columns = ['id', 'city', 'animal', 'furniture', 'hoa', 'rent amount', 'property tax', 'fire insurance'])
+    df.columns = ['Área', 'Quartos', 'Banheiros', 'Vagas de Garagem', 'Andar', 'Total']
+
+    for column in df.columns[:-1]:
+        create_fig(df, column, 'Total')
+    
+    print('Gráficos gerados com sucesso')
 
 
 if __name__ == '__main__':
